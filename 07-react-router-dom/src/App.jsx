639b067e8eba9b0ecc30964d28e8165919574c70
Danielsasa <danielsasa1@hotmail.com>
2025-06-23 19:47:16 -0600

    ✨ se agrego BrowserRouter

--- 07-react-router-dom/src/App.jsx
+++ 07-react-router-dom/src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <BrowserRouter>
+      <Navbar />
+      <AppRoutes />
+      </BrowserRouter>
+    </div>
   )
 }
 
