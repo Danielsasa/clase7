05e9a01750f9225b9ca24fe650be589edf2fc002
Danielsasa <danielsasa1@hotmail.com>
2025-06-25 19:29:58 -0600

    ✨ actualizacion autentificacion

--- 07-react-router-dom/src/routes/AppRoutes.jsx
+++ 07-react-router-dom/src/routes/AppRoutes.jsx
@@ -1,15 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Characters from '../pages/Characters'
-import CharacterDetail from '../pages/CharacterDetail'
+import CharacterDetail from '../pages/Characterdetail'
 import NotFound from '../pages/NotFound'
-const AppRoutes = () => {
+import RequireAuth from '../components/RequireAuth'
+import Secret from '../pages/Secret'
+
+const AppRoutes = ({ isAuth }) => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/characters' element={<Characters />} />
       <Route path='/character/:id' element={<CharacterDetail />} />
       {/* Ruta para manejar páginas no encontradas (404) */}
+      <Route
+        path='/secret' element={
+          <RequireAuth isAuth={isAuth}>
+            <Secret />
+          </RequireAuth>
+      }
+      />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
